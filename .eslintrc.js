ae86ef11b83a841e42a5862e60cb47353515c57c
Daniel <d.lee.2@elsevier.com>
2021-02-20 09:08:32 +0000

    Add boilerplate

---
+++ .eslintrc.js
@@ -0,0 +1,52 @@
+module.exports = {
+  env: {
+    es6: true,
+    node: true,
+  },
+  extends: [
+    'eslint:recommended',
+    'plugin:@typescript-eslint/recommended',
+    'plugin:jest/recommended',
+    'plugin:prettier/recommended',
+    'plugin:react/recommended',
+    'prettier/@typescript-eslint',
+  ],
+  parser: '@typescript-eslint/parser',
+  parserOptions: {
+    ecmaVersion: 2018,
+    sourceType: 'module',
+    ecmaFeatures: {
+      jsx: true,
+    },
+  },
+  plugins: ['@typescript-eslint', 'jest', 'prettier', 'react'],
+  rules: {
+    '@typescript-eslint/explicit-function-return-type': 'error',
+    '@typescript-eslint/interface-name-prefix': 'off',
+    '@typescript-eslint/no-inferrable-types': 'off',
+    '@typescript-eslint/typedef': [
+      'error',
+      {
+        arrayDestructuring: true,
+        arrowParameter: true,
+        memberVariableDeclaration: true,
+        objectDestructuring: true,
+        parameter: true,
+        propertyDeclaration: true,
+        variableDeclarationIgnoreFunction: true,
+      },
+    ],
+    'import/prefer-default-export': 'off',
+    'no-param-reassign': ['error', { props: false }],
+    'prettier/prettier': ['error', { singleQuote: true }],
+    'react/react-in-jsx-scope': 'off',
+    'react/jsx-filename-extension': [1, { extensions: ['.tsx'] }],
+    'spaced-comment': ['error', 'always', { exceptions: ['*'] }],
+  },
+  settings: {
+    'import/resolver': { node: { extensions: ['.js', '.ts', '.tsx'] } },
+    react: {
+      version: 'detect',
+    },
+  },
+};
